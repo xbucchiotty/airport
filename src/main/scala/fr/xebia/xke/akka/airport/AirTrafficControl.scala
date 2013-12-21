5897c9c8684e8235b441b217f8bdceac43df41fe
Xavier Bucchiotty <xbucchiotty@xebia.fr>
2013-12-21 17:25:36 +0100

    Add unimplemented method

--- src/main/scala/fr/xebia/xke/akka/airport/AirTrafficControl.scala
+++ src/main/scala/fr/xebia/xke/akka/airport/AirTrafficControl.scala
@@ -1,10 +1,10 @@
 package fr.xebia.xke.akka.airport
 
-import akka.actor.{ActorRef, Props, Actor}
+import akka.actor.{ActorLogging, ActorRef, Props, Actor}
 import fr.xebia.xke.akka.airport.Command.{Contact, Land}
 import fr.xebia.xke.akka.airport.Event.{Incoming, Landed}
 
-class AirTrafficControl(groundControl: ActorRef) extends Actor {
+class AirTrafficControl(groundControl: ActorRef) extends Actor with ActorLogging {
 
   private val runway = context.actorOf(Props(classOf[Runway], self), "runway")
 
@@ -15,6 +15,10 @@ class AirTrafficControl(groundControl: ActorRef) extends Actor {
 
     case Landed(plane, _) =>
       plane ! Contact(groundControl)
+
+    case Left(_) =>
+      log.warning("TODO IMPLEMENTS ME (air traffic control notified of a plane has left a runway)")
+
   }
 
 }
