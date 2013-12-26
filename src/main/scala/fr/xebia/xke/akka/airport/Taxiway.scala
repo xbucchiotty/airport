ae8c256a00d963d7fc387609a26a6c7f9ea56450
Xavier Bucchiotty <xbucchiotty@xebia.fr>
2013-12-26 08:52:27 +0100

    Queueing the taxiway

--- src/main/scala/fr/xebia/xke/akka/airport/Taxiway.scala
+++ src/main/scala/fr/xebia/xke/akka/airport/Taxiway.scala
@@ -5,62 +5,78 @@ import fr.xebia.xke.akka.airport.Event.{HasEntered, HasLeft, TaxiingToGate, HasP
 import concurrent.duration._
 import languageFeature.postfixOps
 import scala.util.Random
+import scala.collection.immutable.Queue
 
 class Taxiway(capacity: Int, groundControl: ActorRef) extends Actor with ActorLogging {
   assert(capacity > 0)
 
-  def available(free: Int): Receive = planeLeaves(free) orElse {
+  private var queue = Queue.empty[HasParked]
 
+  private var free = capacity
+
+  val acceptNewPlane: Receive = {
     case msg@TaxiingToGate(plane, taxiway, gate) if taxiway == self =>
-
       log.info("Plane <{}> runs on taxiway <{}>", plane.path.name, self.path.name)
+
       groundControl ! HasEntered(plane, self)
 
-      val freePlaces = free - 1
+      this.queue = queue enqueue HasParked(plane, gate)
 
-      if (freePlaces < 1) {
-
+      free -= 1
+      if (free < 1) {
         log.info("Taxiway {} is now full", self.path.name)
         context become full
-
       } else {
-
-        log.info("Remains {} places on taxiway {}", freePlaces, self.path.name)
-        context become available(freePlaces)
-
+        log.info("Remains {} places on taxiway {}", free, self.path.name)
       }
-
-      import context.dispatcher
-      context.system.scheduler.scheduleOnce(taxiingDuration, self, HasParked(plane, gate))
-
   }
 
-  def planeLeaves(free: Int): Receive = {
-    case msg@HasParked(plane, gate) =>
-      log.info("Plane <{}> leaves taxiway <{}>", plane.path.name, self.path.name)
-      groundControl ! HasLeft(plane, self)
-      gate ! msg
-      context become available(free + 1)
-  }
-
-  val full: Receive = planeLeaves(0) orElse {
+  val rejectNewPlane: Receive = {
     case msg@TaxiingToGate(plane, taxiway, _) if taxiway == self =>
       log.error("Plane <{}> runs on a full taxiway <{}>", plane.path.name, self.path.name)
       context stop self
+
   }
 
+  val planeLeaves: Receive = {
+    case this.Tick =>
+      if (queue.nonEmpty) {
+        val (msg, newQueue) = queue.dequeue
+        this.queue = newQueue
+        import msg._
 
-  def receive: Receive = available(capacity)
+        log.info("Plane <{}> leaves taxiway <{}>", plane.path.name, self.path.name)
+        groundControl ! HasLeft(plane, self)
+        gate ! msg
 
-  private def taxiingDuration: FiniteDuration =
-    Duration(
-      Random.nextInt(Taxiway.TAXIING_TIMEOUT),
-      MILLISECONDS
-    )
+        free += 1
+
+        context become available
+      }
+
+      import context.dispatcher
+      context.system.scheduler.scheduleOnce(randomTaxiingDuration, self, Tick)
+
+  }
+
+  import context.dispatcher
+
+  context.system.scheduler.scheduleOnce(randomTaxiingDuration, self, Tick)
+
+  def available: Receive = planeLeaves orElse acceptNewPlane
+
+  def full: Receive = planeLeaves orElse rejectNewPlane
+
+  def receive: Receive = available
+
+  private def randomTaxiingDuration: FiniteDuration =
+    Duration(Random.nextInt(Taxiway.TAXIING_TIMEOUT), MILLISECONDS)
+
+
+  case object Tick
 
 }
 
 object Taxiway {
   val TAXIING_TIMEOUT = 1000
-
 }
--- src/test/scala/fr/xebia/xke/akka.airport/TaxiwaySpec.scala
+++ src/test/scala/fr/xebia/xke/akka.airport/TaxiwaySpec.scala
@@ -1,6 +1,10 @@
 package fr.xebia.xke.akka.airport
 
+import akka.testkit.TestProbe
+import concurrent.duration._
+import fr.xebia.xke.akka.airport.Event.{HasEntered, HasLeft}
 import fr.xebia.xke.akka.airport.specs.{TaxiwaySpecs, PlaneSpecs, ActorSpecs}
+import languageFeature.postfixOps
 
 class TaxiwaySpec extends TaxiwaySpecs with PlaneSpecs with ActorSpecs {
 
@@ -68,31 +72,76 @@ class TaxiwaySpec extends TaxiwaySpecs with PlaneSpecs with ActorSpecs {
           }
         }
       }
+  }
 
-      `Given an actor system` {
-        implicit system =>
+  `Given an actor system` {
+    implicit system =>
+
+      `Given a probe` {
+        groundControl => {
 
           `Given a probe` {
-            groundControl => {
+            gate =>
 
-              `Given a probe` {
-                gate =>
+              `Given a taxiway of capacity`(10, groundControl.ref) {
+                taxiway =>
 
-                  `Given a taxiway of capacity`(10, groundControl.ref) {
-                    taxiway =>
+                  `Given a probe` {
+                    plane =>
 
-                      `Given a probe` {
-                        plane =>
+                      `When a plane enters the taxiway`(plane, taxiway, gate.ref) {
 
-                          `When a plane enters the taxiway`(plane, taxiway, gate.ref) {
-
-                            `Then plane should be out of taxiway within timeout`(taxiway, groundControl, gate, plane.ref)
-                          }
+                        `Then plane should be out of taxiway within timeout`(taxiway, groundControl, gate, plane.ref)
                       }
                   }
               }
-            }
           }
+        }
       }
   }
+
+
+  `Given an actor system` {
+    implicit system =>
+
+      `Given a probe` {
+        groundControl => {
+
+          `Given a probe` {
+            gate =>
+
+              `Given a taxiway of capacity`(10, groundControl.ref) {
+                taxiway =>
+
+                  "Given 5 planes" - {
+
+                    val planes = for (i <- 0 until 5)
+                    yield TestProbe()
+
+                    "When the start taxiing in an order" - {
+                      planes.foreach {
+                        plane =>
+                          plane send(taxiway, Event.TaxiingToGate(plane.ref, taxiway, gate.ref))
+                      }
+
+                      "Then they should leave the taxiway in the same order" in {
+                        groundControl.within(10 seconds) {}
+                        planes.foreach {
+                          plane =>
+                            groundControl expectMsg HasEntered(plane.ref, taxiway)
+                        }
+
+                        planes.foreach {
+                          plane =>
+                            groundControl expectMsg HasLeft(plane.ref, taxiway)
+                        }
+                      }
+                    }
+                  }
+              }
+          }
+        }
+      }
+  }
+
 }
