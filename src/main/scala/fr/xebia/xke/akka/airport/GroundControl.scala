68fe5c61f7fe723ac1329493bb02a58a284bc795
Xavier Bucchiotty <xbucchiotty@xebia.fr>
2013-12-26 17:23:03 +0100

    Heavy refactor Cleaning GroundControl

--- src/main/scala/fr/xebia/xke/akka/airport/GroundControl.scala
+++ src/main/scala/fr/xebia/xke/akka/airport/GroundControl.scala
@@ -2,7 +2,7 @@ package fr.xebia.xke.akka.airport
 
 import akka.actor.{ActorRef, ActorLogging, Actor}
 import fr.xebia.xke.akka.airport.Command.TaxiAndPark
-import fr.xebia.xke.akka.airport.Event.{HasLeft, HasEntered, Incoming}
+import fr.xebia.xke.akka.airport.Event.{HasParked, HasLeft, HasEntered, Incoming}
 
 class GroundControl(taxiway: ActorRef, gate: ActorRef) extends Actor with ActorLogging {
 
@@ -11,9 +11,15 @@ class GroundControl(taxiway: ActorRef, gate: ActorRef) extends Actor with ActorL
       sender ! TaxiAndPark(taxiway, gate)
 
     case HasEntered =>
+      val plane = sender
       log.warning("TODO IMPLEMENTS ME (ground control notified of a plane has entered a taxiway)")
 
     case HasLeft =>
+      val plane = sender
+      log.warning("TODO IMPLEMENTS ME (ground control notified of a plane has left a taxiway)")
+
+    case HasParked=>
+      val plane = sender
       log.warning("TODO IMPLEMENTS ME (ground control notified of a plane has left a taxiway)")
   }
 }
