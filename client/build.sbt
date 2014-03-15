cb15731acd72f67246958e505284928a69e49d98
Jean Helou <jean.helou@gmail.com>
2014-03-15 08:14:47 +0100

    Easily run the client from the console

--- client/build.sbt
+++ client/build.sbt
@@ -15,3 +15,5 @@ libraryDependencies ++= Seq(
 )
  
 fork in run := true
+
+mainClass in (Compile,run) := Some("Launcher")
--- project/Build.scala
+++ project/Build.scala
@@ -3,13 +3,11 @@ import Keys._
 
 object AirportBuild extends Build {
 
-  lazy val root = Project(id = "airport",
-    base = file(".")) aggregate(messages, client)
 
-  lazy val messages = Project(id = "messages",
-    base = file("messages"))
+  lazy val root = Project(id = "airport", base = file(".")).aggregate(messages, client).settings(  addCommandAlias("start", ";project client; run"):_*  )
 
-  lazy val client = Project(id = "client",
-    base = file("client")) dependsOn(messages)
+  lazy val messages = Project(id = "messages", base = file("messages"))
+
+  lazy val client = Project(id = "client", base = file("client")).dependsOn(messages)
 
 }
